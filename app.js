6b744d32de98ddee419717ce790b2e2ca00a09e1
Leong B.S <leongbs34@gmail.com>
2023-07-18 05:10:49 +0800

    test workflow

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const postsRoutes = require("./routes/posts");
-const userRoutes = require("./routes/user");
+const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
 mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGODBUSER}:${process.env.MONGODBPASSWORD}@cluster0.1iq91.mongodb.net/mean-project?retryWrites=true&w=majority`
-  )
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch(() => {
-    console.log("Error connecting to DB");
-  });
+	.connect(
+		`mongodb+srv://${process.env.MONGODBUSER}:${process.env.MONGODBPASSWORD}@cluster0.1iq91.mongodb.net/mean-project?retryWrites=true&w=majority`
+	)
+	.then(() => {
+		console.log('Connected to database!');
+	})
+	.catch(() => {
+		console.log('Error connecting to DB');
+	});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("images")));
+app.use('/images', express.static(path.join('images')));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+	);
+	next();
 });
 
-app.use("/api/posts", postsRoutes);
-app.use("/api/user", userRoutes);
+app.use('/api/posts', postsRoutes);
+app.use('/api/user', userRoutes);
 
 module.exports = app;
